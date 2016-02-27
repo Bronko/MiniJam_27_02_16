8c6efc8e2633ca4f71cdeb6a4f7eb229fb20492e
Bronko <pascal.schroeter@freenet.de>
2016-02-27 18:36:56 +0100

    controls!

--- Project Fall/Assets/src/PlayerPhysics.cs
+++ Project Fall/Assets/src/PlayerPhysics.cs
@@ -5,8 +5,13 @@ public class PlayerPhysics : MonoBehaviour {
 
     public GameObject Planet;
 
+    private Rigidbody myRB;
+
+    public float force = 48f;
+
     public void Awake()
     {
+        myRB = GetComponent<Rigidbody>();
        // Physics.gravity = new Vector3(0, -1.0F, 0);
     }
 
@@ -17,6 +22,43 @@ public class PlayerPhysics : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        Vector3 offset = transform.position - Planet.transform.position;
+        offset.Normalize();
+
+      
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+          //  transform.Rotate(0, -force , 0, Space.World);
+        }
+
+        
+
 	}
+
+    public void FixedUpdate()
+    {
+        Vector3 offset = transform.position - Planet.transform.position;
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.localRotation *= Quaternion.Euler(0, -force, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.localRotation *= Quaternion.Euler(0, force, 0);
+        }
+
+        myRB.AddRelativeForce(0, 0, 40f);
+
+        Vector3 vel = myRB.velocity;
+        vel.Normalize();
+        offset = vel - transform.position;
+        transform.LookAt(Planet.transform, vel);
+
+        transform.Rotate(Vector3.left, 90);
+    }
 }
---
+++ Project Fall/Assets/src/RotationComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.src
+{
+
+    public class RotationComponent : MonoBehaviour
+    {
+        private Rigidbody myRB;
+
+        public Transform Reference;
+
+        public void Awake()
+        {
+            myRB = GetComponent<Rigidbody>();
+        }
+
+        public void FixedUpdate()
+        {
+            
+        }
+    }
+}
